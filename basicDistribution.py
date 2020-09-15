be95681ff9ad30bc679a5484d203fcfc16d5c7f7
fabianlachmann <fabian.lachmann2@gmail.com>
2020-09-15 21:21:11 +0200

    added basicDistribution.py

---
+++ basicDistribution.py
@@ -0,0 +1,36 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from mpl_toolkits import mplot3d
+from scipy.interpolate import interp2d
+
+size = [0.46,0.66,0.89,1.15,1.45,1.85,2.55,3.5,4.5,5.75,7.25,9,11,13,15,16.5]
+Messgrössen = [[0.38,0.54],[0.54,0.78],[0.78,1],[1,1.3],[1.3,1.6],[1.6,2.1],[2.1,3],[3,4],[4,5],[5,6.5],[6.5,8],[8,10],[10,12],[12,14],[14,16],[16,100]]
+
+
+def basicDistribution(data,k):
+    x = []
+    y = []
+
+    for row in data:
+        for i in range(16):
+            x.append(size[k+i])
+            y.append(float(row[k+i])* 1000000)# siehe Physik unserer Umwelt Die Atmosphäre S.18 (math.log10(Messgrössen[i][1])-math.log10(Messgrössen[i][0]))
+
+        plt.plot(x, y, marker='o')
+
+        # Format Plot
+        plt.title("Mittlere Konzentration", fontsize=24)#"Aerosol Konzentration am " + row[0] + "." + row[1] + ". um " + row[2] + " Uhr"
+        plt.xlabel('Diameter $[\mu m]$', fontsize=16)
+        plt.ylabel("$dN/dlogD\;[m^{-3}]$", fontsize=16)
+        plt.yscale('log')
+        plt.xscale('log')
+        plt.ylim(0,10**7)
+        plt.tick_params(axis='both', which='major', labelsize=16)
+        plt.show()
+
+        x = []
+        y = []
---
+++ main.py
@@ -0,0 +1,33 @@
+# Hier File auswählen und dann zu funtion in anderem file schicken
+from basicDistribution import *
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from mpl_toolkits import mplot3d
+from scipy.interpolate import interp2d
+
+
+data = []
+k=3
+with open(askopenfilename()) as csv_file:#öffnet das csv-file
+    csv_reader = csv.reader(csv_file, delimiter=',') #initialisiert den Reader fürs csv
+    for row in csv_reader:
+        if row == []:
+            continue
+        elif row[0] == "Month":
+            k = 3
+            continue
+        elif row[0] == "Station":
+            k = 8
+            continue
+        elif row[0] == "Timecodes":
+            k = 4
+            continue
+        data.append(row)
+
+basicDistribution(data,k)
